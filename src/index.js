5e9b186168219c50e17ff895c988869436684163
Harsha RamachandraRao <8248042+harshar1987@users.noreply.github.com>
2018-10-01 09:42:48 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { render } from "react-dom";
 
-import HomePage from "./pages/HomePage";
+import App from "./app";
 
 const rootElement = document.getElementById("root");
-render(<HomePage />, rootElement);
+render(<App />, rootElement);
